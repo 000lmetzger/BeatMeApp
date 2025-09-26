e85d701137ba7617d15239c4e0696747eaeb0abe
Felix Ruediger <felix.ruediger05@googlemail.com>
2025-09-26 11:15:02 +0200

    fix fields to not show null

--- backend/src/main/java/de/beatme/challenge/Challenge.java
+++ backend/src/main/java/de/beatme/challenge/Challenge.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Challenge {
     private String challengeId;
-    private String challengeName;
-    private String challengeDescription;
+    private String challenge;
+    private String description;
 }
 
